59b8d7c0641766d2bd89b2ece271ca50fa19ee88
Frank Baethge <frank@baethge.info>
2017-05-08 23:02:17 +0200

    das htmlO (html-Objekt) muss nicht mehr als Parameter übergeben werden die Reihenfolge im splanX-Array ist jetzt in Zeilen, das vereinfacht die Erstellung der Tabelle weitere Schönheitskorrekturen

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created by frank on 04.05.17.
  */
@@ -7,7 +6,7 @@
  * fragt den Stundenplan ab und produziert eine entsprechende HTML-Tabelle
  */
 
-function getStundenplanTable(htmlO, cb) {
+function getStundenplanTable(cb) {
 	var splanf = [];
 	var splans = [];
 	for(var i = 0; i<12; i++) {
@@ -16,49 +15,35 @@ function getStundenplanTable(htmlO, cb) {
 	}
 
 	$.getJSON('https://mns.topsch.net/vapp/mns_vapp_api/', function (data) {
-        // initialisiere Stundenplan-Array splan mit leeren Werten
-        // trage alle gefundenen Daten ein
-        $.each(data, function (key, val) {
-            console.debug(val);
-            if (val.f == '1') {
-                splanf[val.tag -1][val.stunde -1] = val.bezeichnung;
-            } else {
-                splans[val.tag -1][val.stunde -1] = val.bezeichnung;
-            }
-        })
+		// initialisiere Stundenplan-Array splan mit leeren Werten
+		// trage alle gefundenen Daten ein
+		$.each(data, function (key, val) {
+			if (val.f == '1') {
+				splanf[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+			} else {
+				splans[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+			}
+		})
 
-// Nachbearbeitung
-        var splanTHead = '<thead><tr><td></td><td>Mo</td><td>Die</td><td>Mi</td><td>Do</td><td>Fr</td></tr></thead>';
-        // TODO: Inhalte eintragen
+		// Nachbearbeitung
+		var splanTHead = '<thead><tr><td></td><td>Mo</td><td>Die</td><td>Mi</td><td>Do</td><td>Fr</td></tr></thead>';
+		// TODO: Tabellen aus f und s vereinen
+		// TODO: Texte in den Zellen auf das nötigste kürzen
+		// TODO: leere Zeilen in der Tabelle nicht anzeigen
 
+		var splan = '';
+		for (var i = 0; i < 12; i++) {
+			splan += '<tr><td>' + (i + 1) + '</td><td>' + splans[i].join('</td><td>') + '</td></tr>';
+		}
 
-        var splan = [];
-        for(var i = 0;i<12;i++) {
-            splan[i] = '<tr><td>'+ (i+1) +'</td><td>' + splans[0][i] + '</td><td>' + splans[1][i] + '</td><td>' + splans[2][i] + '</td><td>' + splans[3][i] + '</td><td>' + splans[4][i] + '</td></tr>';
-        }
-        console.debug(splanf,splans);
-
-        cb(htmlO,'<table>' + splanTHead + splan[0] + splan[1] + splan[2] + splan[3] + splan[4] + splan[5] + splan[6] + splan[7] + splan[8] + splan[9] + splan[10] + splan[11] +	'</table>');
-    });
-
-//	afterload (splanf,splans);
-
-
-
-}
-/*
- afterload(){
-    // Nachbearbeitung
-    var splanTHead = '<thead><tr><td></td><td>Mo</td><td>Die</td><td>Mi</td><td>Do</td><td>Fr</td></tr></thead>';
-    // TODO: Inhalte eintragen
-    var Zeile1 = '<tr><td>1.</td> <td>' +  splanf[1][1] +'</td><td>'+splanf[1][2]+'</td><td>'+splanf[1][3]+'</td><td>'+splanf[1][4]+'</td><td>'+splanf[1][5]+'</td></tr>';
-    console.debug(splanf,splans);
-    '<table>' + splanTHead + Zeile1 +	'</table>';
-}
-*/
-	$( document ).ready(function() {
-		getStundenplanTable($('#StundenplanTabelle'),function(htmlO, tabelle){
-			htmlO.append(tabelle);
-		});
+		cb('<table>' + splanTHead + '<tbody>'+ splan + '</tbody></table>');
 	});
+
+}
+
+$(document).ready(function () {
+	getStundenplanTable(function (tabelle) {
+		$('#StundenplanTabelle').append(tabelle);
+	});
+});
 
