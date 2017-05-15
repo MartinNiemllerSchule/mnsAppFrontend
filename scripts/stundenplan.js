884944ccb2ca6d37fc6cfd42041ff87d3c593391
JanikBuecher <janik.buecher@gmx.de>
2017-05-15 09:58:14 +0200

    Add files via upload

--- scripts/stundenplan.js
+++ scripts/stundenplan.js
@@ -7,23 +7,37 @@
  */
 
 function getStundenplanTable(cb) {
+	var splanfQ = [];
+	var splansQ = [];
 	var splanf = [];
 	var splans = [];
 	for(var i = 0; i<12; i++) {
 		splanf[i] = [undefined,undefined,undefined,undefined,undefined];
 		splans[i] = splanf[i];
-	}
+	} // initialisiere Stundenplan-Array splan mit leeren Werten
 
-	$.getJSON('https://mns.topsch.net/vapp/mns_vapp_api/', function (data) {
-		// initialisiere Stundenplan-Array splan mit leeren Werten
+	$.getJSON('https://mns.topsch.net/vapp/mns_vapp_api/', function(data) {
+
 		// trage alle gefundenen Daten ein
-		$.each(data, function (key, val) {
-			if (val.f == '1') {
-				splanf[val.stunde - 1][val.tag - 1] = val.bezeichnung;
-			} else {
-				splans[val.stunde - 1][val.tag - 1] = val.bezeichnung;
-			}
-		})
+
+
+
+
+                $.each(data[0], function (key, val) {
+                    if (val.f == '1') {
+                        splanf[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+                    } else {
+                        splans[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+                    }
+
+                })
+
+
+
+
+
+
+
 
 		// Nachbearbeitung
 		var splanTHead = '<thead><tr><td></td><td>Mo</td><td>Die</td><td>Mi</td><td>Do</td><td>Fr</td></tr></thead>';
@@ -38,6 +52,7 @@ function getStundenplanTable(cb) {
 
 		cb('<table>' + splanTHead + '<tbody>'+ splan + '</tbody></table>');
 	});
+
 
 }
 
