c0cb9b677e7cf306828b45181ad1c462b134347a
JanikL <janikliebrecht@gmail.com>
2017-05-18 10:24:09 +0200

    Add files via upload

--- scripts/vertretungsplan.js
+++ scripts/vertretungsplan.js
@@ -1,3 +1,30 @@
 /**
  * Created by Janik.Liebrecht on 09.05.2017.
  */
+
+
+function getVertretungsplanTabelle(cb) {
+    var vplan = [];
+
+    $.getJSON('https://mns.topsch.net/vapp/mns_vapp_api/', function (data) {
+
+        $.each(data[1], function (key, val) {
+
+            vplan.push([val.tag, val.stunde, val.kurs, val.raum, val.lehrer, val.info]);
+        });
+        var vplanTHead = '<thead><tr><td>Tag</td><td>Stunde</td><td>Kurs</td><td>Raum</td><td>Lehrer</td><td>Info</td></tr></thead>';
+        var vplanTBody = '';
+
+        for (var i = 0; i < vplan.length; i++) {
+            vplanTBody += '<tr><td>' + vplan[i].join('</td><td>') +'</td></tr>';
+        }
+
+        cb('<table>' + vplanTHead + vplanTBody + '</table>');
+    });
+}
+
+$(document).ready(function () {
+    getVertretungsplanTabelle(function (tabelle) {
+        $('#VertretungsplanTabelle').append(tabelle);
+    });
+});
