bff45d34145c752f62430bcca38f70f9b9492b0f
felixblang <felix.blang@outlook.com>
2017-05-20 12:51:08 +0200

    Stundenplan update

--- scripts/stundenplan.js
+++ scripts/stundenplan.js
@@ -7,58 +7,95 @@
  */
 
 function getStundenplanTable(cb) {
-	var splanfQ = [];
-	var splansQ = [];
-	var splanf = [];
-	var splans = [];
-	for(var i = 0; i<12; i++) {
-		splanf[i] = [undefined,undefined,undefined,undefined,undefined];
-		splans[i] = splanf[i];
-	} // initialisiere Stundenplan-Array splan mit leeren Werten
+    var splanf = [];
+    var splans = [];
+    var splanft = [];
+    var splanst = [];
+    for(var i = 0; i<12; i++) {
+        splanf[i] = ["","","","",""];
+        splans[i] = ["","","","",""];
+        splanft[i] = ["","","","",""];
+        splanst[i] = ["","","","",""];
+    }
+    //console.debug(splans);
+    $.getJSON('https://mns.topsch.net/vapp/mns_vapp_api/', function (data) {
+        // initialisiere Stundenplan-Array splan mit leeren Werten
+        // trage alle gefundenen Daten ein
 
-	$.getJSON('https://mns.topsch.net/vapp/mns_vapp_api/', function(data) {
+        $.each(data[0], function (key, val) {
+            if (val.f == '1') {
+                splanf[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+                splanft[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+            }if (val.s == '1'){
+                splans[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+                splanst[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+            }
+        })
 
-		// trage alle gefundenen Daten ein
 
+        // Nachbearbeitung
+        var splanTHead = '<thead><tr><td></td><td>Mo</td><td>Di</td><td>Mi</td><td>Do</td><td>Fr</td></tr></thead>';
+        // TODO: Tabellen aus f und s vereinen
+        // TODO: Texte in den Zellen auf das nötigste kürzen
 
+        console.debug("splans",splans);
+        console.debug("splanss",splanst);
 
-
-                $.each(data[0], function (key, val) {
-                    if (val.f == '1') {
-                        splanf[val.stunde - 1][val.tag - 1] = val.bezeichnung;
-                    } else {
-                        splans[val.stunde - 1][val.tag - 1] = val.bezeichnung;
+        for (var i = 1; i < 12; i++) {
+            for (var j = 0; j < 5; j++) {
+                if (splans[i][j] == splans[i-1][j] && splans[i-1][j] !== "") {
+                    splanst[i-1][j] = '<td rowspan="2" style="border: 1px solid black;">' + splans[i-1][j] + '</td>';
+                    splanst[i][j] = "";
+                }else{
+                    if(i>1 && splans[i-2][j] == splans[i-1][j] && splans[i-1][j] !== "") {
+                        splanst[i - 1][j] = "";
+                        splanst[i][j] = '<td style="border: 1px solid black;">' + splans[i][j] + '</td>';
+                    }else {
+                        splanst[i - 1][j] = '<td style="border: 1px solid black;">' + splans[i - 1][j] + '</td>';
+                        splanst[i][j] = '<td style="border: 1px solid black;">' + splans[i][j] + '</td>';
                     }
+                }
+                if (splanf[i][j] == splanf[i-1][j] && splanf[i-1][j] !== "") {
+                    splanft[i-1][j] = '<td rowspan="2" style="border: 1px solid black;">' + splanf[i-1][j] + '</td>';
+                    splanft[i][j] = "";
+                }else{
+                    if(i>1 && splanf[i-2][j] == splanf[i-1][j] && splanf[i-1][j] !== "") {
+                        splanft[i - 1][j] = "";
+                        splanft[i][j] = '<td style="border: 1px solid black;">' + splanf[i][j] + '</td>';
+                    }else {
+                        splanft[i - 1][j] = '<td style="border: 1px solid black;">' + splanf[i - 1][j] + '</td>';
+                        splanft[i][j] = '<td style="border: 1px solid black;">' + splanf[i][j] + '</td>';
+                    }
+                }
+            }
+        }
+        console.debug("splans",splans);
+        console.debug("splanss",splanst);
 
-                })
+        var splangerade = '';
+        for (var i = 0; i < 12; i++) {
+            if(splans[i][0] == "" && splans[i][1] == "" && splans[i][2] == "" && splans[i][3] == "" &&splans[i][4] == ""){
+            }else{
+                splangerade += '<tr><td style="border: 1px solid black;">' + (i + 1) + '</td>' + splanst[i] + '</tr>';
+            }
+        }
 
+        var splanungerade = '';
+        for (var i = 0; i < 12; i++) {
+            if(splanf[i][0] == "" && splanf[i][1] == "" && splanf[i][2] == "" && splanf[i][3] == "" &&splanf[i][4] == ""){
+            }else{
+                splanungerade += '<tr><td style="border: 1px solid black;">' + (i + 1) + '</td>' + splanft[i] + '</tr>';
+            }
+        }
 
-
-
-
-
-
-
-		// Nachbearbeitung
-		var splanTHead = '<thead><tr><td></td><td>Mo</td><td>Die</td><td>Mi</td><td>Do</td><td>Fr</td></tr></thead>';
-		// TODO: Tabellen aus f und s vereinen
-		// TODO: Texte in den Zellen auf das nötigste kürzen
-		// TODO: leere Zeilen in der Tabelle nicht anzeigen
-
-		var splan = '';
-		for (var i = 0; i < 12; i++) {
-			splan += '<tr><td>' + (i + 1) + '</td><td>' + splans[i].join('</td><td>') + '</td></tr>';
-		}
-
-		cb('<table>' + splanTHead + '<tbody>'+ splan + '</tbody></table>');
-	});
-
+        cb('<table style="border: 1px solid black;">' + splanTHead + '<tbody>'+ splangerade + '</tbody></table>');
+        cb('<table>' + splanTHead + '<tbody>'+ splanungerade + '</tbody></table>');
+    });
 
 }
 
 $(document).ready(function () {
-	getStundenplanTable(function (tabelle) {
-		$('#StundenplanTabelle').append(tabelle);
-	});
+    getStundenplanTable(function (tabelle) {
+        $('#StundenplanTabelle').append(tabelle);
+    });
 });
-
