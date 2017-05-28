b313ba4a6e8015b69bbd0e40c372099dead86b7f
Frank Baethge <frank@baethge.info>
2017-05-28 22:18:03 +0200

    Verbindung mit der lokalen Datenbank aufnehmen, Daten ablegen und auf gespeicherte Daten reagieren (AutoLogin)

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@
 "use strict";
 var salt = 'sazter45($';
 var urlLogin = 'http://127.0.1.5/index.php';
+var db;
+var loggedIn = false;
 
 /**
  * onDocumentReady
@@ -13,8 +15,43 @@ var urlLogin = 'http://127.0.1.5/index.php';
  *
  */
 $(document).ready(function () {
-	setHandleLogin();
+	if (!loggedIn) {
+		// falls die Datenbank bereits sinnvolle Werte enthält, sollen diese für das Login augenutzt werden
+		if (db) {
+			db.config
+				.get('autoLogin')
+				.then(function (aL) {
+					if (aL == null) {
+						throw 'automatisches Login existiert in der Datenbank nicht';
+					} else {
+						if (aL) {
+							// automatisches Login ausführen
+							console.debug('aL: ' + aL);
+							// TODO: verstecke Login, logIn und zeige ?irgendwas?
+						} else {
+							throw 'automatisches Login ist verboten';
+						}
+					}
+				})
+				.catch(function (error) {
+					console.debug('kein automatisches Login e:' + error);
+					setHandleLogin(); // Login anzeigen
+				});
+		} else console.debug('lokale Datenbank nicht bereit.');
+	}
+
 });
+
+/**
+ * Verbindung mit der lokalen Datenbank herstellen
+ */
+function connectLocalDB() {
+	db = new Dexie("Einstellungen");
+	db.version(1).stores({
+		config:'key,value'
+	});
+}
+connectLocalDB();
 
 /**
  * sendet die Login-Daten und reagiert auf die Antwort
@@ -47,9 +84,26 @@ function sendLoginData(sean, passwort) {
 		data: sendData,
 		success: function(response){
 			console.debug(response);
+			db.config.put({key:'sean', value:sean}).catch(function (error) {
+				console.debug('DB-Error sean: ' + sean + ' e:' + error);
+			});
+			db.config.put({key:'pw', value:passwort}).catch(function (error) {
+				console.debug('DB-Error pw: ' + passwort + ' e:' + error);
+			});
+			db.config.put({key:'loginType', value:'sean'}).catch(function (error) {
+				console.debug('DB-Error loginType=sean e:' + error);
+			});
+			db.config.put({key:'loginDate', value: new Date()}).catch(function (error) {
+				console.debug('DB-Error loginDate e:' + error);
+			});
+			var autoLogin = $("[name='autoLogin']").is(':checked');
+			db.config.put({key:'autoLogin', value: autoLogin}).catch(function (error) {
+				console.debug('DB-Error autoLogin e:' + error);
+			});
+			loggedIn = true;
 		},
 		error: function (response,textStatus,e) {
-			console.debug('kein login', textStatus, e);
+			console.debug('kein login auf dem Server möglich', textStatus, e);
 		}
 	});
 }
