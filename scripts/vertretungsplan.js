c137e1312a261f312c9d0c0514ac7631f32abf58
felixblang <felix.blang@outlook.com>
2017-06-27 15:09:52 +0200

    eigener / gesamter Vertretungsplan

--- scripts/vertretungsplan.js
+++ scripts/vertretungsplan.js
@@ -29,12 +29,35 @@ function getVertretungsplanTabelle(cb) {
             vplanTBody += '<tr><td>' + vplan[i].join('</td><td>') +'</td></tr>';
         }
 
-        cb('<table>' + vplanTHead + '<tbody>' + vplanTBody + '</tbody></table>');
+        cb('<table class="Vertretungsplan tactive" id="diese-woche">' + vplanTHead + '<tbody>' + vplanTBody + '</tbody></table>');
+    });
+}
+function getVertretungsplanTabelleAlle(cb) {
+    var vplan = [];
+
+    db.config.get('vplanAlle').then(function(dataO){
+        var data = dataO.value;
+
+        $.each(data, function (key, val) {
+
+            vplan.push([val.tag, val.stunde, val.bezeichnung, val.raum, val.kuerzel, val.info]);
+        });
+        var vplanTHead = '<thead><tr><td>Tag</td><td>Stunde</td><td>Kurs</td><td>Raum</td><td>Lehrer</td><td>Info</td></tr></thead>';
+        var vplanTBody = '';
+
+        for (var i = 0; i < vplan.length; i++) {
+            vplanTBody += '<tr><td>' + vplan[i].join('</td><td>') +'</td></tr>';
+        }
+
+        cb('<table class="Vertretungsplan" id="nächste-woche">' + vplanTHead + '<tbody>' + vplanTBody + '</tbody></table>');
     });
 }
 
 $(document).ready(function () {
     getVertretungsplanTabelle(function (tabelle) {
+        $('#VertretungsplanTabelle').append(tabelle);
+    });
+    getVertretungsplanTabelleAlle(function (tabelle) {
         $('#VertretungsplanTabelle').append(tabelle);
     });
 });
