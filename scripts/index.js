a9a32c5f9acc12321f1d56b3fb48dc7cd0400191
Frank Baethge <frank@baethge.info>
2017-06-30 22:48:27 +0200

    index.js neu erstellt index.js übernimmt die vorherige Aufgabe von main.js. main.js soll jetzt diejenigen Funktionen bündeln, die in mehreren Seiten benötigt werden.

---
+++ scripts/index.js
@@ -0,0 +1,123 @@
+/**
+ * Zeigt Login-Bildschirm an oder verzweigt bei "eingeloggt bleiben" auf die Startseite
+ */
+
+"use strict";
+var salt = 'sazter45($';
+var urlLogin = 'http://vapp.niemoeller.schule/api/index.php';
+//var urlLogin = 'http://127.0.1.5/index.php';
+var db;
+var loggedIn = false;
+
+/* Datenbank ========================================================= */
+
+/**
+ * Verbindung mit der lokalen Datenbank herstellen
+ */
+function connectLocalDB() {
+	db = new Dexie("Einstellungen");
+	db.version(1).stores({config:'key,value'});
+	$( setHandleLogin ); // Login-Button ausstatten
+
+	db.config
+		.get('autoLogin')
+		.then(function (aL) {
+			// versuche automatisches Login
+			if (aL == null) {
+				throw 'automatisches Login existiert in der Datenbank nicht';
+			} else {
+				if (aL.value) {
+					/* automatisches Login ausführen
+					 * Dazu werden zunächst die Daten aus der Datenbank entnommen und
+					 * der Login vom Server abgerufen.
+					 * Ist der Abruf erfolgreich, wird der aktuelle Stunden und Vertretungsplan als Antwort des Servers
+					 * in der Datenbank gespeichert, wo ihn die anderen Seiten finden und anzeigen können.
+					 */
+					// TODO: verstecke Login, logIn und zeige ?irgendwas?
+					var sendData = 'fname=login&sean=';
+					db.config
+						.get('sean')
+						.then(function (sean) {
+							sendData += sean.value;
+							db.config
+								.get('pw')
+								.then(function (pw) {
+									sendData += '&pw=' + pw.value;
+									$.ajax({
+										url: urlLogin,
+										dataType: 'json',
+										crossDomain: true,
+										data: sendData,
+										success: function(response){
+											loggedIn = true;
+											$(document).ready(handleLogin(response)); // Antwort in DB speichern
+										},
+										error: function (response,textStatus,e) {
+											console.debug('kein login auf dem Server möglich', textStatus, e);
+										}
+									});
+								});
+						});
+				} else {
+					throw 'automatisches Login ist verboten';
+				}
+			}
+		})
+		.catch(function (e) {
+			console.debug('autoLogin unmöglich: ',e);
+			$( setHandleLogin ); // Login-Button ausstatten
+		});
+}
+connectLocalDB();
+
+/**
+ * onDocumentReady
+ * zeigt falls nötig den Login und führt ihn aus anderenfalls wird der Vertretungsplan für diese Woche angezeigt
+ *
+ * wird zur Zeit nicht mehr benötigt (Bereitstellung der Datenbankinformationen ist langsamer als das Laden der Seite
+ $( function () {
+});
+ */
+
+/**
+ * sendet die Login-Daten und reagiert auf die Antwort
+ */
+function setHandleLogin(){
+	$('#loginButton').removeAttr('onsubmit').click(
+		function () {
+			getPasswordHash(sendLoginData);
+		});
+}
+/**
+ * liest das eingegebene Passwort aus und übergibt den Passwort-Hash an das Formular
+ */
+function getPasswordHash(sLD) {
+	var sean = $("[name='sean']").val();
+	var passwort = $("[name='passwort']");
+	var pw = sha512(salt + passwort.val());
+	sLD(sean, pw);
+}
+/**
+ * sendet die Daten für den Login
+ */
+function sendLoginData(sean, passwort) {
+	var sendData = 'fname=login&sean=' + sean + '&pw=' + passwort;
+	$.ajax({
+		url: urlLogin,
+		dataType: 'json',
+		crossDomain: true,
+		data: sendData,
+		success: function(response){
+			db.config.put({key:'sean', value:sean});
+			db.config.put({key:'pw', value:passwort});
+			db.config.put({key:'loginType', value:'sean'});
+			var autoLogin = $("[name='autoLogin']").is(':checked');
+			db.config.put({key:'autoLogin', value: autoLogin});
+			loggedIn = true;
+			handleLogin(response, db);
+		},
+		error: function (response,textStatus,e) {
+			console.debug('kein login auf dem Server möglich', textStatus, e);
+		}
+	});
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,126 +1,8 @@
 /**
- * Zeigt Login-Bildschirm an oder verzweigt bei "eingeloggt bleiben" auf die Startseite
+ * allgemeine Funktionen, die auch in anderen Seiten benötigt werden
  */
 
 "use strict";
-var salt = 'sazter45($';
-var urlLogin = 'http://vapp.niemoeller.schule/api/index.php';
-//var urlLogin = 'http://127.0.1.5/index.php';
-var db;
-var loggedIn = false;
-
-/* Datenbank ========================================================= */
-
-/**
- * Verbindung mit der lokalen Datenbank herstellen
- */
-function connectLocalDB() {
-	db = new Dexie("Einstellungen");
-	db.version(1).stores({config:'key,value'});
-	$( setHandleLogin ); // Login-Button ausstatten
-
-	db.config
-		.get('autoLogin')
-		.then(function (aL) {
-			// versuche automatisches Login
-			if (aL == null) {
-				throw 'automatisches Login existiert in der Datenbank nicht';
-			} else {
-				if (aL.value) {
-					/* automatisches Login ausführen
-					 * Dazu werden zunächst die Daten aus der Datenbank entnommen und
-					 * der Login vom Server abgerufen.
-					 * Ist der Abruf erfolgreich, wird der aktuelle Stunden und Vertretungsplan als Antwort des Servers
-					 * in der Datenbank gespeichert, wo ihn die anderen Seiten finden und anzeigen können.
-					 */
-					// TODO: verstecke Login, logIn und zeige ?irgendwas?
-					var sendData = 'fname=login&sean=';
-					db.config
-						.get('sean')
-						.then(function (sean) {
-							sendData += sean.value;
-							db.config
-								.get('pw')
-								.then(function (pw) {
-									sendData += '&pw=' + pw.value;
-									$.ajax({
-										url: urlLogin,
-										dataType: 'json',
-										crossDomain: true,
-										data: sendData,
-										success: function(response){
-											loggedIn = true;
-											$(document).ready(handleLogin(response)); // Antwort in DB speichern
-										},
-										error: function (response,textStatus,e) {
-											console.debug('kein login auf dem Server möglich', textStatus, e);
-										}
-									});
-								});
-						});
-				} else {
-					throw 'automatisches Login ist verboten';
-				}
-			}
-		})
-		.catch(function (e) {
-			console.debug('autoLogin unmöglich: ',e);
-			$( setHandleLogin ); // Login-Button ausstatten
-		});
-}
-connectLocalDB();
-
-/**
- * onDocumentReady
- * zeigt falls nötig den Login und führt ihn aus anderenfalls wird der Vertretungsplan für diese Woche angezeigt
- *
- * wird zur Zeit nicht mehr benötigt (Bereitstellung der Datenbankinformationen ist langsamer als das Laden der Seite
- $( function () {
-});
- */
-
-/**
- * sendet die Login-Daten und reagiert auf die Antwort
- */
-function setHandleLogin(){
-	$('#loginButton').removeAttr('onsubmit').click(
-		function () {
-			getPasswordHash(sendLoginData);
-		});
-}
-/**
- * liest das eingegebene Passwort aus und übergibt den Passwort-Hash an das Formular
- */
-function getPasswordHash(sLD) {
-	var sean = $("[name='sean']").val();
-	var passwort = $("[name='passwort']");
-	var pw = sha512(salt + passwort.val());
-	sLD(sean, pw);
-}
-/**
- * sendet die Daten für den Login
- */
-function sendLoginData(sean, passwort) {
-	var sendData = 'fname=login&sean=' + sean + '&pw=' + passwort;
-	$.ajax({
-		url: urlLogin,
-		dataType: 'json',
-		crossDomain: true,
-		data: sendData,
-		success: function(response){
-			db.config.put({key:'sean', value:sean});
-			db.config.put({key:'pw', value:passwort});
-			db.config.put({key:'loginType', value:'sean'});
-			var autoLogin = $("[name='autoLogin']").is(':checked');
-			db.config.put({key:'autoLogin', value: autoLogin});
-			loggedIn = true;
-			handleLogin(response);
-		},
-		error: function (response,textStatus,e) {
-			console.debug('kein login auf dem Server möglich', textStatus, e);
-		}
-	});
-}
 
 /**
  * das Login wurde ausgeführt und liefert Daten
@@ -130,13 +12,13 @@ function sendLoginData(sean, passwort) {
  * @param antwort -> JSON-Objekt mit der Rückgabe der bisher relevanten Daten
  *  dabei wird zwischen Lehrern und Schülern unterschieden
  */
-function handleLogin(antwort) {
+function handleLogin(antwort, db, antwortLoginText = 'ok') {
 	console.debug(antwort);
 	// Login war erfolgreich -> Datum des Login speichern
 	db.transaction('rw', db.config, function () {
 		db.config.put({key: 'loginDate', value: new Date()});
 		// Daten in die lokale Datenbank eintragen (falls vorhanden - unterscheidet sich für Schüler und Lehrer)
-		if (antwort.login == 'ok') {
+		if (antwort.login == antwortLoginText) {
 			if ('splan' in antwort) {
 				db.config.put({key: 'splan', value: antwort.splan});
 			}
