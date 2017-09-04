6904dd19e788474131c8090522c068402c5e8fbd
JanikBuecher <janik.buecher@gmx.de>
2017-09-04 15:38:15 +0200

    fix

--- src/scripts/Qr.js
+++ src/scripts/Qr.js
@@ -87,7 +87,7 @@ $(function() {
             var buchNr=  $('#scanner_input').val();
             console.debug(buchNr);
             var sendData = "fname=getBuch&bnr=" + buchNr;
-            conole.debug(sendData);
+            console.debug(sendData);
             $.ajax({
                 url: 'https://vapp.niemoeller.schule/api/index.php',
                 dataType: 'json',
