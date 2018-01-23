eea2398d856de2e0618263ac2adac36e819714ee
Frank Baethge <baethge@niemoeller.schule>
2018-01-23 23:01:20 +0100

    Arbeit auf zwei Datenbanken

---
+++ src/inventur/api/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Verbindungsdaten der Datenbank
+ * und andere Konstanten, die nicht in ein (öffentliches) Verzeichnis kommen sollen
+ *
+ * User: frank
+ * Date: 29.04.17
+ * Time: 22:15
+ */
+define('HOST','localhost');
+define('USER','tlmbibo');
+define('PASS','cTec22o$');
+define('DB', 'test_qr_lmBibo');
+
+define('SALT', 'sazter45($');
+define('TEST', 'tset');
+
+define('LOG', true);
+?>
