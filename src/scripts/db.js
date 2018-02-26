e37e16572214b24e68568d7cd26b3c2342516599
felixblang <felix.blang@outlook.com>
2018-02-26 19:10:26 +0100

    Revert "debug"
    This reverts commit 54fd919759c457892ed4c76f7a49becd154159be.

--- src/scripts/db.js
+++ src/scripts/db.js
@@ -35,7 +35,6 @@ define('db', ['dexie'], function (Dexie) {
                     }
                     // müsste auf IOS
                     if ('splan' in antwort) {
-                        console.debug(antwort.splan);
                         $.each(antwort.splan, function () {
                             db.splan.put({
                                 bezeichnung: this.bezeichnung,
