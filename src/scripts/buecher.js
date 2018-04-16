ac5471f926b003b9b22e56c05c40e5e983ae3967
JanikBuecher <janik.buecher@gmx.de>
2018-04-16 10:31:13 +0200

    removed anschaffungsjahr from buecherliste

--- src/scripts/buecher.js
+++ src/scripts/buecher.js
@@ -17,13 +17,12 @@ requirejs(['./scripts/vapp.js'], function () {
 
 function getBuecherTabelle(db, cb) {
     db.buecher.toArray().then(buecher => {
-        var buecherTHead = '<thead><tr><th>BEAN</th><th>Titel</th><th>Ausleihdatum</th><th>Kurs</th><th>Anschaffungsjahr</th></tr></thead>';
+        var buecherTHead = '<thead><tr><th>BEAN</th><th>Titel</th><th>Ausleihdatum</th><th>Kurs</th></tr></thead>';
         var buecherTBody = '';
         var td = '</td><td>';
         for (var i = 0; i < buecher.length; i++) {
 
-        	buecherTBody += '<tr><td>' + buecher[i].bean + td + buecher[i].titel + td + buecher[i].ausleihdatum + td + buecher[i].kurs
-                + td + buecher[i].anschaffungsjahr + '</td></tr>';
+        	buecherTBody += '<tr><td>' + buecher[i].bean + td + buecher[i].titel + td + buecher[i].ausleihdatum + td + buecher[i].kurs + '</td></tr>';
 
         }
         cb('<table class=" Buecherliste">' + buecherTHead + '<tbody>' + buecherTBody + '</tbody></table>');
