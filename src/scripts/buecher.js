c45ba26fdac0068af1f0a132a0a000ad419ad440
JanikBuecher <janik.buecher@gmx.de>
2018-04-16 10:29:00 +0200

    deleted old buechertable function

--- src/scripts/buecher.js
+++ src/scripts/buecher.js
@@ -14,25 +14,7 @@ requirejs(['./scripts/vapp.js'], function () {
 	});
 });
 
-function getBuecherplanTable(db, cb) {
-	var buecherplan = [];
 
-	// initialisiere Stundenplan-Array splan mit leeren Werten
-	// trage alle gefundenen Daten ein
-	db.buecher.each(function (data) {
-			buecherplan.push([data.bean, data.titel, data.ausleihdatum, data.kurs, data.anschaffungsjahr]);
-		});
-
-		var buecherTHead = '<thead><tr><th>BEAN</th><th>Titel</th><th>Ausleihdatum</th><th>Kurs</th><th>Anschaffungsjahr</th></tr></thead>';
-		setTimeout(function () {
-            var bplan = '';
-            for (var i = 0; i < buecherplan.length; i++) {
-                bplan += '<tr><td>' + buecherplan[i].join('</td><td>') + '</td></tr>';
-            }
-
-            cb('<table class="Buecherliste">' + buecherTHead + bplan + '</table>');
-        },500);
-}
 function getBuecherTabelle(db, cb) {
     db.buecher.toArray().then(buecher => {
         var buecherTHead = '<thead><tr><th>BEAN</th><th>Titel</th><th>Ausleihdatum</th><th>Kurs</th><th>Anschaffungsjahr</th></tr></thead>';
