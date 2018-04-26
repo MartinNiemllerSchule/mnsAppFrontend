0f8912d978ea7b4bc39b41c41452166e0c00e236
Janik.Liebrecht <janikliebrecht@gmail.com>
2018-04-26 10:06:53 +0200

    Vplan Test

--- src/scripts/vertretungsplan.js
+++ src/scripts/vertretungsplan.js
@@ -32,7 +32,7 @@ function getVertretungsplanTabelle(db, cb) {
             var datum2 = new Date(jahr, monat, tag);
 
             if (datum1.getTime() != datum2.getTime()) {
-                vplanTBody += '<tr><th colspan="6">' + tage[datum2.getDay()] + ', ' + datum2.toDateString() + '</th></tr>';
+                vplanTBody += '<tr><th colspan="6">' + tage[datum2.getDay()] + ', ' + datum2.toLocaleDateString() + '</th></tr>';
                 datum1 = datum2;
             }
 
